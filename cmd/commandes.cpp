ecb9aa4771523a4ccaa5e0fdf5827ecd32015f1c
fez <eugenefezeu@gmail.com>
2019-03-15 01:33:40 +0100

    multiline commande

--- cmd/commandes.cpp
+++ cmd/commandes.cpp
@@ -9,46 +9,33 @@
 #include <string>
 #include "header.h"
 #include <fstream>
+#include "commande.h"
+#include "commandes.h"
 
     using namespace std;
 
-    class commandes{
-        string * liste;
-        unsigned int length;
-
-        public:
-            commandes();
-            commandes(unsigned int ,string * );
-            int copy( unsigned int,const string *);
-            int append(const string );
-            int erase(unsigned int);
-            void clear(void);
-            void reverse(void);
-            int replace(unsigned int, unsigned int, string);
-            friend commandes operator+ (commandes, commandes);
-            friend commandes operator+= (commandes, commandes);
-            commandes operator= (commandes);
-    };
+    
     
     commandes::commandes(){
         liste = NULL;
         length = 0;
     };
-    commandes::commandes(unsigned int argc,string* argv){
-        liste = new string[argc];   
+    commandes::commandes(unsigned int argc,commande* argv){
+        liste = new commande[argc];   
         for(length = 0;  length < argc; length ++){
             liste[length] = argv [length];
         }
         
     };
-    int commandes::copy(unsigned int argc, const string * argv){
-        string tampon [length];
+    int commandes::copy(unsigned int argc, const commande * argv){
+        commande tampon [length];
         unsigned int i = 0;
         for( i = 0; i < length; i++){
             tampon[i] = liste[i];
         }
         length += argc;
-        liste =  new string [length]; 
+        delete liste;
+        liste =  new commande [length]; 
         for( i = 0; i < length - argc; i++){
             liste[i] = tampon[i]  ;
         }
@@ -57,14 +44,15 @@
         }
         return 0;
     };
-    int commandes::append(const string  arg){
-        string tampon [length];
+    int commandes::append(const commande  arg){
+        commande tampon [length];
         unsigned int i = 0;
         for( i = 0; i < length; i++){
             tampon[i] = liste[i];
         }
         length ++;
-        liste =  new string [length]; 
+        delete liste;
+        liste =  new commande [length]; 
         for( i = 0; i < length - 1; i++){
             liste[i] = tampon[i]  ;
         }
@@ -74,7 +62,7 @@
         return 0;
     };
     int commandes::erase(unsigned int  position){
-        string tampon [length - 1];
+        commande tampon [length - 1];
         unsigned int i = 0;
         if(position < 0 || position >=length){
             return -1;
@@ -88,7 +76,8 @@
             tampon[i - 1] = liste[i - 1];
         }
         length --;
-        liste =  new string [length]; 
+        delete liste;
+        liste =  new commande [length]; 
         for( i = 0; i < length; i++){
             liste[i] = tampon[i]  ;
         }
@@ -100,24 +89,35 @@
         length = 0;
     };
     void commandes::reverse(void){
-        string tampon [length - 1];
+        commande tampon [length - 1];
         unsigned int i = 0;
         while(  i < length ){
             tampon[ i ] = liste[i];
         }
-        liste =  new string [length];
+        delete liste;
+        liste =  new commande [length];
         for( i = 0; i < length; i++){
             liste[length - i -1] = tampon[i]  ;
         }
     }
-    int commandes::replace(unsigned int st, unsigned int end, string arg){
+    int commandes::execute_all(){
+            
+        unsigned int i(0);
+        while(i<length){
+            liste[i].execute();
+            i++;
+        }
+        
+        return 1;
+    }
+    int commandes::replace(unsigned int st, unsigned int end, commande arg){
         if(st > end){
             return -1;
         }
         if(st < 0 || end >=length){
             return -1;
         }
-        string tampon [ length -(end -st + 1) ];
+        commande tampon [ length -(end -st + 1) ];
         unsigned int i = 0;
        
         while(  i < st ){
@@ -129,7 +129,8 @@
             tampon[i] = liste[i];
             i++;
         }
-        liste =  new string [length -st -end + 2];
+        delete liste;
+        liste =  new commande [length -st -end + 2];
         i = 0;
         while(  i < st ){
             liste[i] = tampon[i];
@@ -144,13 +145,14 @@
         return 0;
     };
     commandes operator+ (commandes l1, commandes l2){
-        string tampon [l1.length];
+        commande tampon [l1.length];
         unsigned int i = 0;
         for( i = 0; i < l1.length; i++){
             tampon[i] = l1.liste[i];
         }
         l1.length += l2.length;
-        l1.liste =  new string [l1.length]; 
+        delete l1.liste;
+        l1.liste =  new commande [l1.length]; 
         for( i = 0; i < l1.length - l2.length; i++){
             l1.liste[i] = tampon[i]  ;
         }
@@ -160,16 +162,54 @@
         return l1;
     }
 
-    commandes commandes::operator= (commandes l1){
-        liste =  new string [l1.length];
-        length = l1.length;
-        unsigned int i = 0;
-        for( i = 0; i < l1.length; i++){
-            liste[i] = l1.liste[i];
+    commandes& commandes::operator= (const commandes & l1){
+        if (this != &l1){
+            delete liste;
+            liste =  new commande [l1.length];
+            length = l1.length;
+            unsigned int i = 0;
+            for( i = 0; i < l1.length; i++){
+                liste[i] = l1.liste[i];
+            }
         }
-        
         return *this;
     }
     commandes operator+= (commandes l1, commandes l2){
         return l1 = l1 + l2;
+    }
+    commandes::commandes(char * line){
+                int bufsize = TOK_BUFSIZE, position = 0;
+        char **tokens = (char **)malloc(bufsize * sizeof(char*));
+        char *token, **tokens_backup;
+
+        if (!tokens) {
+            cerr << "msh: allocation error" << endl;
+            exit(EXIT_FAILURE);
+        }
+
+        token = strtok(line, ";");
+        while (token != NULL) {
+            tokens[position] = token;
+            position++;
+
+            if (position >= bufsize) {
+                bufsize += TOK_BUFSIZE;
+                tokens_backup = tokens;
+                tokens = (char **)realloc(tokens, bufsize * sizeof(char*));
+                if (!tokens) {
+                    free(tokens_backup);
+                    cerr << "msh: allocation error" << endl;
+                    exit(EXIT_FAILURE);
+                }
+            }
+
+            token = strtok(NULL, TOK_DELIM);
+        }
+        int i(0);
+        liste = new commande [position];
+        while(i < position ){
+            liste[i] = commande (tokens[i]);
+            i++;
+        }
+        length = position ;
     }
--- cmd/fonctions.cpp
+++ cmd/fonctions.cpp
@@ -11,6 +11,7 @@
 #include <readline/history.h>
 #include "header.h"
 #include "commande.h"
+#include "commandes.h"
 
     using namespace std;
     
@@ -272,8 +273,8 @@
  
     do {
         line = read_line();
-        commande cmd(line);
-        status = cmd.execute();
+        commandes cmds(line);
+        status = cmds.execute_all();
 
         free(line);
     
